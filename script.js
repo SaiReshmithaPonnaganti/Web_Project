bb19e8377ef47725740799f8b347e00810ff4a33
SaiReshmithaPonnaganti <160369750+SaiReshmithaPonnaganti@users.noreply.github.com>
2024-10-14 17:48:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+document.getElementById('loginForm').addEventListener('submit', function(e) {
+    e.preventDefault();  // Prevent form submission
+    window.location.href = 'blog.html';  // Redirect to the blog page
+});
+
